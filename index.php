1d4c283c7bf6449902ab99345b32f66077baab4a
Farhat Saleem <fsaleemms@live.com>
2021-05-11 16:33:57 -0400

    php changes

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <body>
     <p><?php echo "App Name is: ".getenv("AppName"); ?></p>
     <p><?php 
-    $currentDate = new date();
+    $currentDate = new datetime();
     echo $currentDate->format('Y-m-d H:i:s'); ?></p>
   </body>
 </html>
